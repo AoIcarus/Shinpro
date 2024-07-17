abca964104802aa4ce9183b52e35ab1565221afe
David Veridiano <davidveridiano@gmail.com>
2024-07-17 21:23:32 +0900

    feat: First Commit
    Resolves SHI-11, Resolves #2
    
    Co-authored-by: SporadicToast <me@mgcmacaraeg.com>

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    export let buttonText = "Default";
+
+</script>
+
+<button type="button" class="btn variant-filled">{buttonText}</button>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Button from "$lib/components/Button.svelte";
+
+	$: val = "Input here"
+</script>
+<h1 class="h1">Hello!</h1>
+
+<Button buttonText={val}/>
+<Button buttonText="Hello!2"/>
+<Button buttonText="Hello!3"/>
+
+<input type="text" class="input" bind:value={val}/>
